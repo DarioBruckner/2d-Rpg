993e4b0d710375d2fc462679045c32b3db3fee70
georgreichert <81485014+georgreichert@users.noreply.github.com>
2021-04-08 01:27:09 +0200

    Movement Finetuning
    + Some World Elements

--- New Unity Project/Assets/Scripts/CharacterController.cs
+++ New Unity Project/Assets/Scripts/CharacterController.cs
@@ -4,37 +4,61 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    [Range(0.0f, 1.0f)]
-    public float moveSpeed = 0.1f;
+    [Range(5.0f, 15.0f)]
+    public float moveSpeed;
     Rigidbody2D m_Rigidbody;
-    public float m_Thrust = 20f;
+    [Range(5.0f, 15.0f)]
+    public float m_Thrust;
+    CapsuleCollider2D capsuleCollider;
+    public LayerMask layerMask;
+    float runFactor = 0.0f;
+    bool running = false;
+    [Range(0.0f, 0.99f)]
+    public float dampening;
 
     // Start is called before the first frame update
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            m_Rigidbody.AddForce(transform.up * m_Thrust, ForceMode2D.Impulse);
+        }
     }
 
     private void FixedUpdate()
     {
+        running = false;
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.position = new Vector3(transform.position.x - moveSpeed, transform.position.y, 0);
+            running = true;
+            m_Rigidbody.velocity = new Vector2( - moveSpeed * runFactor, m_Rigidbody.velocity.y);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            transform.position = new Vector3(transform.position.x + moveSpeed, transform.position.y, 0);
+            running = true;
+            m_Rigidbody.velocity = new Vector2(moveSpeed * runFactor, m_Rigidbody.velocity.y);
         }
-        if (Input.GetKey(KeyCode.Space))
+        if (running)
         {
-            m_Rigidbody.AddForce(transform.up * m_Thrust);
+            runFactor += 1.0f - dampening;
+            runFactor = runFactor >= 1.0f ? 1.0f : runFactor;
+        } else
+        {
+            runFactor = 0.0f;
         }
+    }
 
+    private bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position + new Vector3(0, -capsuleCollider.size.y/2, 0), 
+                                new Vector2(capsuleCollider.size.x - 0.1f, 0.1f), 0, Vector2.down, 0.1f, layerMask);
+        return hit.collider != null;
     }
 }
