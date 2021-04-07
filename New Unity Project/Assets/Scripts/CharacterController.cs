7105acbd8a966b84bf92d6589ea092f67e6cebdc
Dario Bruckner <dariobruckner@gmail.com>
2021-04-07 17:05:32 +0200

    added Jump
    added Jump

--- New Unity Project/Assets/Scripts/CharacterController.cs
+++ New Unity Project/Assets/Scripts/CharacterController.cs
@@ -6,11 +6,13 @@ public class CharacterController : MonoBehaviour
 {
     [Range(0.0f, 1.0f)]
     public float moveSpeed = 0.1f;
+    Rigidbody2D m_Rigidbody;
+    public float m_Thrust = 20f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -29,5 +31,10 @@ public class CharacterController : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x + moveSpeed, transform.position.y, 0);
         }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            m_Rigidbody.AddForce(transform.up * m_Thrust);
+        }
+
     }
 }
