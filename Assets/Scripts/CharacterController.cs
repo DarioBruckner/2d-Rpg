6486bb87d25912846dd6d22d051abc3be0f7a9cd
georgreichert <81485014+georgreichert@users.noreply.github.com>
2021-05-03 01:26:42 +0200

    Added character animation
    added character animation in world view

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -12,11 +12,15 @@ public class CharacterController : MonoBehaviour
     CapsuleCollider2D m_BodyCollider;
     public LayerMask layerMask;
     float f_LastAxis = 0.0f;
+    Animator m_Animator;
+    private bool b_Jump = false;
+    private float f_JumpTime = 0.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody2D>();
+        m_Animator = GetComponent<Animator>();
         CapsuleCollider2D[] m_BodyColliders = GetComponents<CapsuleCollider2D>();
         foreach (CapsuleCollider2D col in m_BodyColliders)
         {
@@ -30,9 +34,42 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        if (!b_Jump)
         {
-            m_Rigidbody.AddForce(transform.up * m_Thrust, ForceMode2D.Impulse);
+            if (Input.GetAxis("Horizontal") != 0 && IsGrounded())
+            {
+                m_Animator.SetInteger("animNr", 1);
+            }
+            else if (IsGrounded())
+            {
+                m_Animator.SetInteger("animNr", 0);
+            }
+            if (Input.GetAxis("Horizontal") < 0)
+            {
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else if (Input.GetAxis("Horizontal") > 0)
+            {
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            if (m_Rigidbody.velocity.y < 0 && !IsGrounded())
+            {
+                m_Animator.SetInteger("animNr", 3);
+            }
+            if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+            {
+                m_Animator.SetInteger("animNr", 2);
+                m_Rigidbody.AddForce(transform.up * m_Thrust, ForceMode2D.Impulse);
+                b_Jump = true;
+            }
+        } else
+        {
+            f_JumpTime += Time.deltaTime;
+            if (f_JumpTime >= 0.2f)
+            {
+                f_JumpTime = 0.0f;
+                b_Jump = false;
+            }
         }
     }
 
