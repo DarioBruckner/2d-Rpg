2d4b6399ca12eaabc8d38470d792673e663abc07
Aleks Jevtic <61018659+AleksOfficial@users.noreply.github.com>
2021-05-03 17:53:00 +0200

    Levelloader completed. I believe the other changes are from the build

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+
+public class LevelLoader : MonoBehaviour
+{
+    public object enemy;
+    public Animator transition;
+    private float transitionTime = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.B)) //Bei collision mit einem Player, könnte man hier die Daten speichern des Objektes.
+        {
+            LoadBattle("lol");
+        }
+        if (Input.GetKeyDown(KeyCode.W)) //Wenn zb Der Gegner besiegt is, trigger Load World. World braucht dann eine Klasse, die das Objekt von einem Array entfernt oder so.
+        {
+            LoadWorld();
+        }
+    }
+
+    public void LoadBattle(object other) 
+    { 
+        enemy = other; //Other is the object the player collided with which is stored in enemy. Can be used to construct the fight_scene
+        StartCoroutine(Loader("fight_scene"));
+    }
+
+    public void LoadWorld()
+    {
+        StartCoroutine(Loader("level_0"));
+    }
+    IEnumerator Loader(string scene)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(scene);
+    }
+}
---
+++ Assets/w_WorldComponents.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class w_WorldComponents : MonoBehaviour
+{
+    //Construct world mit Relics/Gegnern/...
+    //Speichere Gegner, die gestorben sind/Quests die erfüllt sind um sie nicht zu spawnen
+    //Bei Spielerkollision mit Gegner, können mithilfe von Tags/ids die Daten der Gegner gefunden werden und so bei der Transitiontobattle übertragen werden
+    //Oder bei der Transition werden jeweils die Daten gespeichert und dann beim constructen der Welt so gesetzt, wie man sie vorgefunden hat. 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+}
