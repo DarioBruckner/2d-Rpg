af7934abd340a2f1248776cb6fa7620f176af249
if20b178 <if20b178@technikum-wien.at>
2021-06-22 15:08:55 +0200

    added constructors to the monster classes

--- Assets/Scripts/Monsters/Drake.cs
+++ Assets/Scripts/Monsters/Drake.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Drake : MonoBehaviour
+public class Drake : MonsterClass
 {
+    public Drake(int lvlUP) : base(lvlUP)
+    {
+        this.s_name = "Drake";
+        this.n_maxHP = (int)Math.Ceiling((double)(n_maxHP * 2));
+        this.n_stdStrength = (int)Math.Ceiling((double)(n_stdStrength * 0.5));
+        this.resetStats();
+    }
     public override void initialize(int lvl)
     {
         base.initialize(lvl);
--- Assets/Scripts/Monsters/Golem.cs
+++ Assets/Scripts/Monsters/Golem.cs
@@ -1,9 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Golem : MonoBehaviour
+public class Golem : MonsterClass
 {
+    public Golem(int lvlUP) : base(lvlUP)
+    {
+        this.s_name = "Golem";
+        this.n_maxHP = (int)Math.Ceiling((double)(n_maxHP * 2));
+        this.n_stdStrength = (int)Math.Ceiling((double)(n_stdStrength * 0.5));
+        this.resetStats();
+    }
     public override void initialize(int lvl)
     {
         base.initialize(lvl);
