cf9aded7a797fc149283c436e0ecc4da7ba6b7fb
georgreichert <81485014+georgreichert@users.noreply.github.com>
2021-07-02 03:47:47 +0200

    added Battle animations for player characters

--- Assets/Scripts/BaseClasses/ItemClass.cs
+++ Assets/Scripts/BaseClasses/ItemClass.cs
@@ -21,4 +21,23 @@ public abstract class ItemClass : MonoBehaviour
     {
         
     }
+
+    public override bool Equals(object other)
+    {
+        // If parameter is null return false.
+        if (other == null)
+        {
+            return false;
+        }
+
+        // If parameter cannot be cast to Person return false.
+        ItemClass item = other as ItemClass;
+        if ((System.Object)item == null)
+        {
+            return false;
+        }
+
+        // Return true if the fields match:
+        return (item.s_itemName == this.s_itemName);
+    }
 }
