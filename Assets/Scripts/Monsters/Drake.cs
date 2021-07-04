a7d8b4be00bf01df5d12fb00df862c13105a27b8
if20b178 <if20b178@technikum-wien.at>
2021-07-05 00:52:02 +0200

    more balancing for drake

--- Assets/Scripts/Monsters/Drake.cs
+++ Assets/Scripts/Monsters/Drake.cs
@@ -11,7 +11,7 @@ public class Drake : MonsterClass
         this.n_stdStrength = (int)Math.Ceiling((double)(this.n_stdStrength * 1));
         this.n_stdAgility = (int)Math.Ceiling((double)(this.n_stdAgility * 1));
         this.n_stdVitality = (int)Math.Ceiling((double)(this.n_stdVitality * 5));
-        this.n_stdMagicalMight = (int)Math.Ceiling((double)(this.n_stdMagicalMight * 1.2));
+        this.n_stdMagicalMight = (int)Math.Ceiling((double)(this.n_stdMagicalMight * 1.5));
         this.n_stdMagicalResistance = (int)Math.Ceiling((double)(this.n_stdMagicalResistance * 0.5));
         this.resetStats();
     }
@@ -25,13 +25,13 @@ public class Drake : MonsterClass
         this.n_stdStrength = (int)Math.Ceiling((double)(this.n_stdStrength * 1));
         this.n_stdAgility = (int)Math.Ceiling((double)(this.n_stdAgility * 1));
         this.n_stdVitality = (int)Math.Ceiling((double)(this.n_stdVitality * 5));
-        this.n_stdMagicalMight = (int)Math.Ceiling((double)(this.n_stdMagicalMight * 1.2));
+        this.n_stdMagicalMight = (int)Math.Ceiling((double)(this.n_stdMagicalMight * 1.5));
         this.n_stdMagicalResistance = (int)Math.Ceiling((double)(this.n_stdMagicalResistance * 0.5));
         this.resetStats();
     }
     public override void attack(ref CharacterClass target)
     {
-        double rawDamage = (this.n_magicalMight / (target.n_magicalResistance)) + 1;
+        double rawDamage = (this.n_magicalMight / ((target.n_magicalResistance/5))) + 1;
         System.Random rng = new System.Random();
         double crit = rng.Next(100) * ((this.n_agility / 100) + 1);
         if (crit > 90)
